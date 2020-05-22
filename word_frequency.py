be6741cdf3e8c50a004ed9833b28010a192c7742
Blake <blakeevans336@gmail.com>
2020-05-22 10:07:28 -0400

    commit

--- word_frequency.py
+++ word_frequency.py
@@ -4,10 +4,38 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+import string
+punctuation = string.punctuation
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+    file = open(file)
+    text = file.read()
+    found_words = []
+    nopunc = ""
+
+    for punc in text:
+        if punc not in punctuation:
+            nopunc = nopunc + punc
+    lower_text = nopunc.lower()
+    print (lower_text)
+
+
+    for word in strings:
+        if word not in stop_words:
+            found_words.append(word)
+    
+    sort_words = sorted(found_words)
+    
+    duplicate_words = dict()
+
+    for word in sort_words:
+        if word in duplicate_words:
+            duplicate_words[word] += 1
+        else:
+            duplicate_words[word] = 1
+    duplicate_words = { key:value for key, value in duplicate_words.items() if value > 1}
+
+
 
 
 if __name__ == "__main__":
