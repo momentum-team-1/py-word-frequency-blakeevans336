8a3fa39a77b310f3fa0f8297aa70e800d8f31312
Blake <blakeevans336@gmail.com>
2020-05-27 09:38:28 -0400

    commit

--- word_frequency.py
+++ word_frequency.py
@@ -1,45 +1,97 @@
+import string
+
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
+    'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with'
 ]
 
-import string
-punctuation = string.punctuation
 
-def print_word_freq(file):
-    file = open(file)
-    text = file.read()
-    found_words = []
-    nopunc = ""
+class FileReader:
+    def __init__(self, filename):
+        self.filename = filename
 
-    for punc in text:
-        if punc not in punctuation:
-            nopunc = nopunc + punc
-    lower_text = nopunc.lower()
-    print (lower_text)
+    def read_contents(self):
+        """
+        This should read all the contents of the file
+        and return them as one string.
+        """
+        opened_file = open(self.filename)
+        self.text = opened_file.read()
+        return self.text
 
 
-    for word in strings:
-        if word not in stop_words:
-            found_words.append(word)
+class WordList:
+    def __init__(self, text):
+        self.text = text
+
+    def extract_words(self):
+        """
+        This should get all words from the text. This method
+        is responsible for lowercasing all words and stripping
+        them of punctuation.
+        """
+        no_punctuation = ""
+        for character in self.text:
+            if character not in string.punctuation:
+                no_punctuation = no_punctuation + character
+        
+        self.new_text = no_punctuation.lower().split()
+
+        return self.new_text
+
+    def remove_stop_words(self):
+        """
+        Removes all stop words from our word list. Expected to
+        be run after extract_words.
+        """
+        self.filtered_text = [text for text in self.new_text if not text in STOP_WORDS]
+
+        return self.filtered_text
+
+    def get_freqs(self):
+        """
+        Returns a data structure of word frequencies that
+        FreqPrinter can handle. Expected to be run after
+        extract_words and remove_stop_words. The data structure
+        could be a dictionary or another type of object.
+        """
+        word_freq = {}
+        for word in self.filtered_text:
+            word_freq[word] = word_freq.get(word, 0) +1
+
+        return word_freq
+
+class FreqPrinter:
+    def __init__(self, freqs):
+        self.freqs = freqs
+
+    def print_freqs(self):
+        """
+        Prints out a frequency chart of the top 10 items
+        in our frequencies data structure.
+        Example:
+          her | 33   *********************************
+        which | 12   ************
+          all | 12   ************
+         they | 7    *******
+        their | 7    *******
+          she | 7    *******
+         them | 6    ******
+         such | 6    ******
+       rights | 6    ******
+        right | 6    ******
+        """
+        word_freq = {key: value for key, value in self.freqs.items() if value >=6}
     
-    sort_words = sorted(found_words)
-    
-    duplicate_words = dict()
+        freq_text = sorted(word_freq.items(), key=lambda seq:seq[0], reverse=False)
 
-    for word in sort_words:
-        if word in duplicate_words:
-            duplicate_words[word] += 1
-        else:
-            duplicate_words[word] = 1
-    duplicate_words = { key:value for key, value in duplicate_words.items() if value > 1}
-
-
-
+        for key, value in freq_text:
+            print (key.rjust(15) , ' | ', str(value).ljust(2), value * ('*'))
 
 if __name__ == "__main__":
     import argparse
+    import sys
     from pathlib import Path
 
     parser = argparse.ArgumentParser(
@@ -49,7 +101,12 @@ if __name__ == "__main__":
 
     file = Path(args.file)
     if file.is_file():
-        print_word_freq(file)
+        reader = FileReader(file)
+        word_list = WordList(reader.read_contents())
+        word_list.extract_words()
+        word_list.remove_stop_words()
+        printer = FreqPrinter(word_list.get_freqs())
+        printer.print_freqs()
     else:
         print(f"{file} does not exist!")
-        exit(1)
+        sys.exit(1)
